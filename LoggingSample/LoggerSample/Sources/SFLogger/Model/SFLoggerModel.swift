1a0c9995bb26c2683b03dadcd47994527c41b646
rei <49666219+rei-sf7@users.noreply.github.com>
2023-09-12 03:14:02 +0900

    Add files via upload

--- LoggingSample/LoggerSample/Sources/SFLogger/Model/SFLoggerModel.swift
+++ LoggingSample/LoggerSample/Sources/SFLogger/Model/SFLoggerModel.swift
@@ -3,6 +3,7 @@ import UIKit
 protocol SFLoggerModelProtocol: AnyObject
 {
     var FilePath: String? { get }
+    var FileFullPath: String? { get }
     init(_ locale: SFLoggerModel.LogLocale)
     func createLogMessage(_ message: String, logType: SFLoggerModel.LogType, error: Error?, file: String, function: String, line: Int) -> String
     func writeLogToFile(_ message: String)
@@ -17,12 +18,22 @@ final class SFLoggerModel: SFLoggerModelProtocol
     
     private var deviceType: DeviceType = .None
     
-    private var fileName: String = "myLogFile.log"
+    private var fileName: String? = nil
     
     private var filePath: String? = nil
     
-    public var FilePath: String? {
-        get {
+    public var FileFullPath: String?
+    {
+        get
+        {
+            return "\(self.filePath!)/\(self.fileName!)"
+        }
+    }
+    
+    public var FilePath: String?
+    {
+        get
+        {
             return self.filePath
         }
     }
@@ -58,11 +69,52 @@ final class SFLoggerModel: SFLoggerModelProtocol
 #else
         self.deviceType = .RealDevice
 #endif
-        do {
-            try self.ensureDocumentsDirectoryExists()
-        } catch let error as NSError {
-            fatalError("エラーが発生しました: \(error.localizedDescription)")
+        
+        self.fileName = self.setLogFileName()
+        
+        self.filePath = self.setLogFilePath()
+    }
+    
+    private func setLogFilePath() -> String! {
+        var documentsUrl: URL? = self.getDocumentsDirectory()
+        if documentsUrl == nil
+        {
+            do {
+                try FileManager.default.createDirectory(at: .documentsDirectory, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                fatalError("ドキュメントディレクトリを作成できませんでした: \(error.localizedDescription)")
+            }
+            documentsUrl = self.getDocumentsDirectory()
         }
+        // ログディレクトリの存在チェック
+        var documentsLogUrl: URL? = self.getDocumentsLogDirectory()
+        if documentsLogUrl == nil
+        {
+            do {
+                try FileManager.default.createDirectory(at: .documentsDirectory.appendingPathComponent("Logs"), withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                fatalError("ログディレクトリを作成できませんでした: \(error.localizedDescription)")
+            }
+            documentsLogUrl = self.getDocumentsLogDirectory()
+        }
+        return documentsLogUrl!.path
+    }
+    
+    
+    private func setLogFileName() -> String {
+        return "\(self.generateDateYmd())_\(self.setBundleName()).log"
+    }
+    
+    private func setBundleName() -> String {
+        let bundleName = Bundle.main.object(forInfoDictionaryKey: kCFBundleNameKey as String) as? String ?? "appLog"
+        return "\(bundleName)"
+    }
+    
+    private func generateDateYmd() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        let dateString = dateFormatter.string(from: Date())
+        return "\(dateString)"
     }
     
     private func nowDateTime() -> String
@@ -131,29 +183,62 @@ final class SFLoggerModel: SFLoggerModelProtocol
         return logMessage
     }
     
-    private func getDocumentsDirectory() -> URL? {
-        if let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+    private func getDocumentsDirectory() -> URL?
+    {
+        if let documentsDirectory: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        {
             return documentsDirectory
         }
         return nil
     }
     
-    private func ensureDocumentsDirectoryExists() throws
+    private func getDocumentsLogDirectory() -> URL?
+    {
+        
+        if let documentsLogDirectory: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Logs")
+        {
+            var isDirectory: ObjCBool = false
+            if FileManager.default.fileExists(atPath: documentsLogDirectory.path, isDirectory: &isDirectory) {
+                if isDirectory.boolValue {
+                    return documentsLogDirectory
+                }
+            }
+        }
+        return nil
+    }
+    
+    private func getApplicationSupportDirectory() -> URL?
+    {
+        if let appSupportDirectory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first {
+            return appSupportDirectory
+        }
+        return nil
+    }
+    
+    private func ensureApplicationSupportDirectoryExists() throws -> String
+    {
+        guard let applicationSupportDirectory: URL = self.getApplicationSupportDirectory() else
+        {
+            fatalError("Application Supportディレクトリを取得できませんでした。")
+        }
+        return applicationSupportDirectory.appendingPathComponent(self.fileName!).path
+    }
+    
+    private func ensureDocumentsDirectoryExists() throws -> String
     {
         // ドキュメントディレクトリの存在チェック
         guard let documentsDirectory: URL = self.getDocumentsDirectory() else
         {
             fatalError("ドキュメントディレクトリを取得できませんでした。")
         }
-        self.filePath = documentsDirectory.appendingPathComponent(self.fileName).path
+        return documentsDirectory.appendingPathComponent(self.fileName!).path
     }
     
     private func createLogFile() throws
     {
-        // ファイル作成
-        if !FileManager.default.fileExists(atPath: self.filePath!)
+        if !FileManager.default.fileExists(atPath: self.FileFullPath!)
         {
-            if !FileManager.default.createFile(atPath: self.filePath!, contents: nil, attributes: nil)
+            if !FileManager.default.createFile(atPath: self.FileFullPath!, contents: nil, attributes: nil)
             {
                 fatalError("ファイルを作成できませんでした。")
             }
@@ -171,7 +256,7 @@ final class SFLoggerModel: SFLoggerModelProtocol
         case .None:
             fatalError("先にinit()で初期化してください。")
         case .Simulator:
-            if let fileHandle = FileHandle(forWritingAtPath: self.filePath!)
+            if let fileHandle = FileHandle(forWritingAtPath: self.FileFullPath!)
             {
                 fileHandle.seekToEndOfFile()
                 if let messageData = "\(message)\n".data(using: .utf8) {
